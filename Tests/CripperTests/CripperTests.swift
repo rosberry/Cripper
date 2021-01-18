33d8ad31e498a97194fa2ea21fecd082894277de
ntunin <nikolay.tyunin@rosberry.com>
2021-01-18 10:33:23 +0600

    make tests runnable

--- Tests/CripperTests/CripperTests.swift
+++ Tests/CripperTests/CripperTests.swift
@@ -6,7 +6,7 @@ final class CripperTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Cripper().text, "Hello, World!")
+        // TODO: Write tests
     }
 
     static var allTests = [
